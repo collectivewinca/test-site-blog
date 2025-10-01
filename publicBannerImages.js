a1137740764ee0c646b58f9fa442e7a31c79af31
collectivewinca <159792879+collectivewinca@users.noreply.github.com>
2025-10-01 20:18:12 +0530

    chore: scaffold publicBannerImages.js

---
+++ publicBannerImages.js
@@ -0,0 +1,20 @@
+// Public banner images list
+// This file exports the available banner images for use across the application
+
+export const BANNER_IMAGES = [
+  '/banners/banner1.webp',
+  '/banners/banner2.webp',
+  '/banners/banner3.webp',
+  '/banners/banner4.webp',
+  '/banners/banner5.webp',
+  '/banners/banner6.webp',
+  '/banners/banner7.webp',
+  '/banners/banner8.webp',
+]
+
+// Default export for easier importing
+export default BANNER_IMAGES
+
+export const USER_BANNER_IMAGES = [
+    
+]
